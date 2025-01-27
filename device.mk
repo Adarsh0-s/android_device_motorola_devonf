fd23c6b8112d45bb79e2dcb024a5fe69ca0415a4
Mashopy <eliasgheeraert@gmail.com>
2025-01-27 13:00:17 +0530

    devonf: Remove leftovers of OMX media codecs XMLs
    Signed-off-by: Adarsh0-s <jeff19whisper@gmail.com>

--- device.mk
+++ device.mk
@@ -266,7 +266,6 @@ PRODUCT_PACKAGES += \
     libcodec2_soft_common.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_c2_shim1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_shim1.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \

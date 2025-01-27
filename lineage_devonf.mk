646018b62ac876e8655a757d0655ab61e34be673
Adarsh0-s <jeff19whisper@gmail.com>
2025-01-27 13:46:23 +0000

    devonf: Update to U1TNS34.82-12-7-8
    * Adapt to RisingOS
    
    Signed-off-by: Adarsh0-s <jeff19whisper@gmail.com>

--- lineage_devonf.mk
+++ lineage_devonf.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/motorola/devonf/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_devonf
+PRODUCT_NAME := lineage_devonf
 PRODUCT_DEVICE := devonf
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
@@ -23,7 +23,17 @@ PRODUCT_MODEL := moto g73 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
+TARGET_ENABLE_BLUR := true
+WITH_GMS := false
+RISING_MAINTAINER := Adarsh
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=devonf \
-    BuildDesc="devonf_g_sys-user 12 U1TNS34.82-12-7-4 00d08 release-keys" \
-BuildFingerprint=motorola/devonf_g_sys/devonf:14/U1TNS34M.82-12-7-4/00d08:user/release-keys
+    BuildDesc="devonf_g_sys-user 12 U1TNS34.82-12-7-8 4f333 release-keys" \
+BuildFingerprint=motorola/devonf_g_sys/devonf:14/U1TNS34M.82-12-7-8/4f333:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Dimensity 930" \
+    RisingMaintainer="Adarsh"
